2909cda930820731baee78fa719d238f25318685
Chinmay Shalawadi <shalawadi.chinmay@gmail.com>
2022-08-28 22:50:53 -0600

    working

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-filesdir=$1
-searchstr=$2
+writefile=$1
+writestr=$2
 
-if [ -z "$filesdir" ] || [ -z "$searchstr" ]
+if [ -z "$writefile" ] || [ -z "$writestr" ]
   then
     echo "One of the parameter not supplied"
     exit 1
 fi
 
-if [[ ! -f "$filesdir" ]]
+if [[ ! -f "$writefile" ]]
   then
-    $(mkdir -p "$(dirname "${filesdir}")")
+    $(mkdir -p "$(dirname "${writefile}")")
 fi
 
-$(echo "$searchstr" > "$(dirname "${filesdir}")"/"$(basename "${filesdir}")")
+$(echo "$writestr" > "$(dirname "${writefile}")"/"$(basename "${writefile}")")
 echo "file directory has been created I think"
 
 exit 0
