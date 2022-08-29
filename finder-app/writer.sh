91865e92ae2dc8a8ec04a4d3d960062afd8d1079
Chinmay Shalawadi <shalawadi.chinmay@gmail.com>
2022-08-28 23:31:59 -0600

    comments added to writer

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -9,19 +9,23 @@
 writefile=$1
 writestr=$2
 
+# check if both paramerts are present
 if [ -z "$writefile" ] || [ -z "$writestr" ]
   then
     echo "One or both of the parameter(s) not supplied"
     exit 1
 fi
 
+#check if directory path exists, if not create
 if [[ ! -f "$writefile" ]]
   then
     $(mkdir -p "$(dirname "${writefile}")")
 fi
 
+# Wrtie contents to file
 $(echo "$writestr" > "${writefile}")
 
+# Check if the fileis successfully created
 if [[ ! -f "$writefile" ]]
   then
     echo "File not created, Error Occured!"
